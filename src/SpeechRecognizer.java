37ee688f6eb3c860ddfb8c34b3f09298cf2b584f
alextsu <alextsu@gmail.com>
2014-03-01 14:25:57 -0500

    Updated

---
+++ src/SpeechRecognizer.java
@@ -0,0 +1,104 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+public class SpeechRecognizer {
+	private Map<String, Map<String, Integer>> wordMap; // 1) the type of speech it is 2) the word 3) the frequency
+	
+	public SpeechRecognizer() {
+		wordMap = new HashMap<String, Map<String, Integer>>();
+	}
+	
+	public void training() throws Exception {
+		BufferedReader words = new BufferedReader(new FileReader("inputs/test-words.txt")); 
+		BufferedReader tags = new BufferedReader(new FileReader("inputs/test-tags.txt")); 
+		
+		String wordLine = new String();
+		String tagLine = new String();
+		
+		ArrayList<String> wordList = new ArrayList<String>();
+		ArrayList<String> tagList = new ArrayList<String>();
+		Set<String> tagSet= new HashSet<String>();
+		
+		String individualWord = "";
+		String individualTag = "";
+		while ((wordLine = words.readLine()) != null) { //passage
+			wordLine = wordLine + " ";
+			for(int i = 0; i < wordLine.length(); i++) { //line 				
+				if(wordLine.charAt(i) == ' ' ) { //word
+					wordList.add(individualWord);
+					individualWord = "";
+				}
+				if(!Character.isAlphabetic(wordLine.charAt(i)) && Character.isDigit(wordLine.charAt(i)) ) {	
+					wordList.add(Character.toString(wordLine.charAt(i)));
+					i++;
+				}
+				else {
+					String temp = new String();
+					temp = individualWord + wordLine.charAt(i);
+					if(temp.equals(" ")) {
+						temp = "";
+					}
+					individualWord = temp;
+				} 
+			}
+		}		
+		while ((tagLine = tags.readLine()) != null) { //passage
+			for(int i = 0; i < tagLine.length(); i++) { //line 
+				if(tagLine.charAt(i) == ' ' ) { //word
+					tagList.add(individualTag);
+					tagSet.add(individualTag);
+					individualTag = "";
+				}
+				if(tagLine.charAt(i) == '.') {
+					tagList.add(Character.toString(tagLine.charAt(i)));
+					tagSet.add(Character.toString(tagLine.charAt(i)));
+					i++;
+				}
+				else {
+					String temp = new String();
+					temp = individualTag + tagLine.charAt(i);
+					if(temp.equals(" ")) {
+						temp = "";
+					}
+					individualTag = temp;
+				}
+			}
+		}
+		/*for(String s : tagSet) {
+			System.out.println(s);
+		} */
+		for(int i = 0; i < wordList.size(); i ++) {
+				System.out.println(i + " " + wordList.get(i) + "//" + tagList.get(i));
+		}  
+		for(String tag : tagSet) {
+			Map<String, Integer> temp = new HashMap<String, Integer>();
+			for(int i = 0; i < tagList.size(); i++) {
+				if(tagList.get(i).equals(tag)) {
+					if(!temp.containsKey(wordList.get(i))) {
+						temp.put(wordList.get(i), 1);
+					}
+					else {
+						int frequency = temp.get(wordList.get(i)) +1;
+						temp.put(wordList.get(i), frequency);
+					}
+				}
+			}
+			wordMap.put(tag, temp);
+		} 
+		
+		for(String t : wordMap.keySet()) {
+			System.out.println(t + "=" + wordMap.get(t));
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		SpeechRecognizer s = new SpeechRecognizer();
+		s.training();
+	}
+}
