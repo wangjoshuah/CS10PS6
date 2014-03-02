c6acf95724d904a2ec39c42f968e46179f4e865e
wangjoshuah <joshua.h.wang.15@dartmouth.edu>
2014-03-01 19:12:04 -0500

    comments done

--- src/SpeechRecognizer.java
+++ src/SpeechRecognizer.java
@@ -7,34 +7,50 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Sudi speech recognizer
+ * @author Alex Tsu '14 and Josh Wang '15 for Dartmouth CS 10 14W
+ */
 
 public class SpeechRecognizer {
+	
+	//private variables that are the maps for words and targets to their frequencies
 	private Map<String, Map<String, Integer>> wordMap; // 1) the type of speech it is 2) the word 3) the frequency
 	private Map<String, Map<String, Integer>> tagMap; // 1) the tag 2) the tag of its next word 3) the frequency of that next word
 	
+	/**
+	 * Constructor
+	 * initializes the word map and the tag map
+	 */
 	public SpeechRecognizer() {
-		wordMap = new HashMap<String, Map<String, Integer>>();
-		tagMap = new HashMap<String, Map<String, Integer>>();
+		wordMap = new HashMap<String, Map<String, Integer>>(); //start wordMap as a hashmap
+		tagMap = new HashMap<String, Map<String, Integer>>(); //also start tagMap
 	}
 	
+	/**
+	 * Read in our word and tag values to tell our speech recognizer how to function
+	 * @throws Exception		incorrect file input
+	 */
 	public void training() throws Exception {
-		BufferedReader words = new BufferedReader(new FileReader("inputs/Brown-words.txt")); 
-		BufferedReader tags = new BufferedReader(new FileReader("inputs/Brown-tags.txt")); 
+		//create readers for all the word in puts and the tag inputs
+		BufferedReader words = new BufferedReader(new FileReader("inputs/Brown-words.txt")); //where do we get our words from input document 
+		BufferedReader tags = new BufferedReader(new FileReader("inputs/Brown-tags.txt"));   //get our tags from other input doc
 		
-		String wordLine = new String();
-		String tagLine = new String();
+		String wordLine = new String(); //string that takes each line of the words file
+		String tagLine = new String();  //string that takes each line of the tags  file
 		
-		ArrayList<String> wordList = new ArrayList<String>();
-		ArrayList<String> tagList = new ArrayList<String>();
-		Set<String> tagSet= new HashSet<String>();
+		ArrayList<String> wordList = new ArrayList<String>();	//list of words keeping track of indices
+		ArrayList<String> tagList = new ArrayList<String>();	//list of tags  keeping track of indices to compare with wordlist
+		Set<String> tagSet= new HashSet<String>();				//set  of tags to put later into our map as the keySet
 		
-		String individualWord = "";
-		String individualTag = "";
+		String individualWord = "";	//keep track of what are individual words in the word input
+		String individualTag = "";	//keep track of what are individual tags  in the tag  input
 		
+		int upperBound = 3000; //keep track of how many lines we want to go through
+		int counter = 0; //start a counter to count how many lines we've read
 		
-		int counter = 0;
-		
-		while ((wordLine = words.readLine()) != null) { //passage
+		//----------------------------------------- read over this algorithm so it's absolutely correct
+		while ((wordLine = words.readLine()) != null) { //while we can continue reading through the
 			wordLine = wordLine + " ";
 			for(int i = 0; i < wordLine.length(); i++) { //line 
 				if(wordLine.charAt(i) == ' ' ) { //word
@@ -61,11 +77,13 @@ public class SpeechRecognizer {
 				} 
 			}
 			counter++;
-			if(counter == 3000) {
+			if(counter == upperBound) {
 				break;
 			}
 		}	
 		counter = 0;
+		
+		//---------------------------------------------- also read this algorithm
 		while ((tagLine = tags.readLine()) != null) { //passage
 			for(int i = 0; i < tagLine.length(); i++) { //line 
 				if(tagLine.charAt(i) == ' ' ) { //word
@@ -83,21 +101,29 @@ public class SpeechRecognizer {
 				}
 			}
 			counter++;
-			if(counter == 3000) {
+			if(counter == upperBound) {
 				break;
 			}
 		}
+		
+		
+		/*
+		 * print functions
+		 */
+		
+		//print out the sizes of the groups
 		System.out.println(tagList.size() + " " + wordList.size());
-		/*for(String s : tagSet) {
-			System.out.println(s);
-		} */
+
+		//print out tags and words
 		for(int i = 0; i < tagList.size(); i ++) {
 				System.out.println(i + " " + wordList.get(i) + "//" + tagList.get(i));
 		}  
-		for(String tag : tagSet) {
-			Map<String, Integer> temp = new HashMap<String, Integer>();
-			for(int i = 0; i < tagList.size(); i++) {
-				if(tagList.get(i).equals(tag)) {
+		
+		//create the has map of tags to words to frequencies **************************************efficiency problem
+		for(String tag : tagSet) { //for each tag
+			Map<String, Integer> temp = new HashMap<String, Integer>(); //create a map with that tag as the key and another map of words and frequencies as value
+			for(int i = 0; i < tagList.size(); i++) { //for all the tags in the list
+				if(tagList.get(i).equals(tag)) { 
 					if(!temp.containsKey(wordList.get(i))) {
 						temp.put(wordList.get(i), 1);
 					}
